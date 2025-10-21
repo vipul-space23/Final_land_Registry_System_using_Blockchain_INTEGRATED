696053fa1d12a4d2eefbfd9956cc14ac7346170d
vipul-space23 <vipulpatil2500@gmail.com>
2025-10-21 17:45:10 +0530

    added

--- Backend/middlewares/uploadMiddleware.js
+++ Backend/middlewares/uploadMiddleware.js
@@ -1,34 +1,3 @@
-// import multer from 'multer';
-// import path from 'path';
-
-// const storage = multer.diskStorage({
-//   destination: './uploads/',
-//   filename: function (req, file, cb) {
-//     cb(
-//       null,
-//       `aadhaar-${req.user.id}-${Date.now()}${path.extname(file.originalname)}`
-//     );
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 2000000 }, // 2MB limit
-//   fileFilter: function (req, file, cb) {
-//     const filetypes = /jpeg|jpg|png|pdf/;
-//     const extname = filetypes.test(
-//       path.extname(file.originalname).toLowerCase()
-//     );
-//     const mimetype = filetypes.test(file.mimetype);
-//     if (mimetype && extname) {
-//       return cb(null, true);
-//     } else {
-//       cb(new Error('Error: PDF, JPG, or PNG files only!'));
-//     }
-//   },
-// });
-
-// export default upload;
 import multer from 'multer';
 import path from 'path';
 
